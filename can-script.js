165ba69251dc754393e19024672416f14835505d
cdavy22 <christopherdavy90@gmail.com>
2022-10-20 10:10:47 +0200

    20/10/2022

--- can-script.js
+++ can-script.js
@@ -2,7 +2,7 @@
 var products;
 
 // utiliser fetch pour récupérer le JSON, et signaler toute erreur qui se produit dans l’opération fetch
-fetch('products.json').then(function (response) {
+fetch('produits.json').then(function (response) {
   if (response.ok) {
     response.json().then(function (json) {
       products = json;
@@ -18,6 +18,7 @@ function initialize() {
   // saisir les éléments d’interface que nous devons manipuler
   var category = document.querySelector('#category');
   var searchTerm = document.querySelector('#searchTerm');
+  var nutriscore = document.querySelector('#nutriscore');
   var searchBtn = document.querySelector('button');
   var main = document.querySelector('main');
  
@@ -44,9 +45,10 @@ function initialize() {
     } else {
       // mettre à jour l’enregistrement de la dernière catégorie et du terme de recherche
       lastCategory = category.value;
+      lastnutriscore = nutriscore.value
       lastSearch = searchTerm.value.trim();
       // sélectionner tous les produits, puis les filtrer par la recherche
-      if (category.value === 'All') {
+      if (category.value === 'tous') {
         categoryGroup = products;
         selectProducts();
       } else {
@@ -73,7 +75,7 @@ function initialize() {
       var lowerCaseSearchTerm = searchTerm.value.trim().toLowerCase();
       // Vérification des termes recherché celon la categorie
       for (var i = 0; i < categoryGroup.length; i++) {
-        if (categoryGroup[i].name.indexOf(lowerCaseSearchTerm) !== -1) {
+        if (categoryGroup[i].nom.indexOf(lowerCaseSearchTerm) !== -1) {
           finalGroup.push(categoryGroup[i]);
         }
       }
@@ -96,53 +98,66 @@ function initialize() {
       main.appendChild(para);
     } else {
       for (var i = 0; i < finalGroup.length; i++) {
-        fetchBlob(finalGroup[i]);
+        showProduct(finalGroup[i]);
       }
     }
   }
 
-  // fetch pour récupérer l’image des produits,
-  function fetchBlob(product) {
-    // construire le chemin URL des images à partir de la propriété product.image
-    var url = 'images/' + product.image;
-    fetch(url).then(function (response) {
-      if (response.ok) {
-        //une URL interne temporaire en convertisant le blob
-        response.blob().then(function (blob) {
-          var objectURL = URL.createObjectURL(blob);
-          showProduct(objectURL, product);
-        });
-      } else {
-        console.log('Network request for "' + product.name + '" image failed with response ' + response.status + ': ' + response.statusText);
-      }
-    });
-  }
-
   // Afficher les produits dans l’élément 
-  function showProduct(objectURL, product) {
+  function showProduct(product) {
     // Creation des sections d'élément
+    var url = 'images/' + product.image;
     var section = document.createElement('section');
     var heading = document.createElement('h2');
     var para = document.createElement('p');
     var image = document.createElement('img');
-
-    // afficher l'icone de produit correct
+    var txtnutriscore = document.createElement('h3');
+    var nutriscore = document.createElement('span');
+    // afficher l'icone de product correct
     section.setAttribute('class', product.type);
 
     // Remplacé par la majuscule le premier caractère
-    heading.textContent = product.name.replace(product.name.charAt(0), product.name.charAt(0).toUpperCase());
+    heading.textContent = product.nom.replace(product.nom.charAt(0), product.nom.charAt(0).toUpperCase());
 
     // Fixer le prix à 2 décimales
-    para.textContent = '$' + product.price.toFixed(2);
+    para.textContent = '€' + product.prix.toFixed(2);
 
     // Définir la source de l’élément 
-    image.src = objectURL;
-    image.alt = product.name;
+    image.src = url;
+    image.alt = product.nom;
+    
+    // mise en couleur par apport au nutriscore 
+    
+    nutriscore.textContent = product.nutriscore
+    if (nutriscore.textContent == 'A') {
+      nutriscore.style.backgroundColor = '#196f3d'
+    }
+
+    else if (nutriscore.textContent == 'B') {
+      nutriscore.style.backgroundColor = '#52be80'
+    }
+
+    else if (nutriscore.textContent == 'C') {
+      nutriscore.style.backgroundColor = '#f1c40f'
+    }
+
+    else if (nutriscore.textContent == 'D') {
+      nutriscore.style.backgroundColor = '#dc7633'
+    }
+
+    else if (nutriscore.textContent == 'E') {
+      nutriscore.style.backgroundColor = '#c0392b'
+    }
+
+
+    txtnutriscore.textContent = 'nutriscore :'
 
     // Ajouter les éléments au DOM, pour ajouter le produit à l’interface utilisateur
     main.appendChild(section);
     section.appendChild(heading);
     section.appendChild(para);
     section.appendChild(image);
+    section.appendChild(txtnutriscore);
+    txtnutriscore.appendChild(nutriscore);
   }
 }
